c38cb589df858ec606166433495b7412428ae132
Akhansha Prashil <57935894+Akhansha@users.noreply.github.com>
2020-11-12 16:37:14 +0530

    Update App.java

--- src/main/java/com/cognizant/truyum/App.java
+++ src/main/java/com/cognizant/truyum/App.java
@@ -14,7 +14,7 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
-        List<MenuItem> menuItems = (List<MenuItem>) ctx.getBean("menuItems");
+        List<MenuItem> menuItems = (List<MenuItem>) ctx.getBean("menuItemList");
         for(MenuItem item : menuItems) {
         	System.out.println(item);
         }
